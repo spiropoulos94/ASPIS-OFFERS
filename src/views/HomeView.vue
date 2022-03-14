d7567282fa48742dd0475845c82a764e648a7f9f
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2022-03-14 21:16:00 +0200

    added element ui

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,50 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>ASPIS Offers</h1>
+    <el-row>
+      <el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+    </el-row>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+
+let hotels = [
+  {
+    name: "Kernos Beach",
+    email: "kernos@beach.gr",
+    img: "https://images.pexels.com/photos/261169/pexels-photo-261169.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    name: "The Artemis Palace",
+    email: "kernos@beach.gr",
+    img: "https://images.pexels.com/photos/261169/pexels-photo-261169.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    name: "Knossos Royal",
+    email: "mail@test.gr",
+    img: "https://images.pexels.com/photos/261169/pexels-photo-261169.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    name: "Knossos Royal",
+    email: "mail@test.gr",
+    img: "https://images.pexels.com/photos/261169/pexels-photo-261169.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+];
 
 export default {
   name: "HomeView",
-  components: {
-    HelloWorld,
+  components: {},
+  data() {
+    return {
+      hotels,
+    };
   },
 };
 </script>
