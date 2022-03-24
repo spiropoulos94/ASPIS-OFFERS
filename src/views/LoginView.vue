21f7cca4856c764da7fa82cf75b60c06139d5943
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2022-03-24 23:11:33 +0200

    custom persisted store logic

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="login">
     <!-- <el-button @click="$store.commit('login')">Login</el-button> -->
+    <div class="logo-wrapper">
+      <img class="logo-img" src="../../public/assets/images/aspis_logo.png" />
+    </div>
   </div>
 </template>
 
@@ -13,3 +16,10 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+.logo-img {
+  width: 200px;
+  height: 200px;
+}
+</style>
