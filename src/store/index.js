4a92e123f7a0fd779bc758fb49b56f98597c2b92
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2022-03-24 22:21:43 +0200

    custom persisted store logic

--- src/store/index.js
+++ src/store/index.js
@@ -4,10 +4,28 @@ import router from "@/router";
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {
-    userIsLoggedIn: false,
-  },
+const storage = window.sessionStorage;
+
+const persistedState = (storage) => {
+  const state = storage.getItem("state");
+
+  if (JSON.parse(state)) {
+    return JSON.parse(state);
+  } else {
+    return null;
+  }
+};
+
+const updatePersistedState = (storage) => {
+  storage.setItem("state", JSON.stringify(store.state));
+};
+
+let initialState = {
+  userIsLoggedIn: false,
+};
+
+let store = new Vuex.Store({
+  state: persistedState(storage) || initialState,
   getters: {
     isUserLoggedIn(state) {
       return state.userIsLoggedIn;
@@ -16,13 +34,17 @@ export default new Vuex.Store({
   mutations: {
     login(state) {
       state.userIsLoggedIn = true;
+      updatePersistedState(storage);
       router.push("/");
     },
     logout(state) {
       state.userIsLoggedIn = false;
+      updatePersistedState(storage);
       router.push("/login");
     },
   },
   actions: {},
   modules: {},
 });
+
+export default store;
